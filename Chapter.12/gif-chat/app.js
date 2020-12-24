efe1ee426d726ea39f4add471339d45d6e976eb4
KangSage <ksage@knou.ac.kr>
2020-12-24 13:52:31 +0900

    Chapter.12 - Simple websocket example

--- Chapter.12/gif-chat/app.js
+++ Chapter.12/gif-chat/app.js
@@ -22,12 +22,12 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COKKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COKKIE_SECRET,
+    secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
